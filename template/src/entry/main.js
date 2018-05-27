1546960ae0bb945429613a490b05643488b012db
cuiyang <cuiyang673308817@gmail.com>
2018-05-27 11:43:50 +0800

    update main.js for eslint

--- template/src/entry/main.js
+++ template/src/entry/main.js
@@ -4,19 +4,16 @@
 
 import Vue from 'vue'
 import App from 'views/app'
-
 {{#router}}
 // 引入vue-router
 import router from '../router'
 {{/router}}
-
 {{#baobabui}}
 // 引入内部组件库
 import 'baobab-ui/lib/baobab-ui.css'
 import BaobabUI from 'baobab-ui'
 Vue.use(BaobabUI)
 {{/baobabui}}
-
 {{#fastclick}}
 // 引入fastclick
 import FastClick from 'fastclick'
