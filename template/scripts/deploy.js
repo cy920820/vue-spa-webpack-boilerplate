6d00352ea725329dbe63c31e5cc2a3248cd61a31
cuiyang <cuiyang673308817@gmail.com>
2018-10-17 17:00:39 +0800

    fixed deploy-kit

--- template/scripts/deploy.js
+++ template/scripts/deploy.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack')
 const config = require('./config/prod')
+const DeployPlugin = require('deploy-kit/plugins/sftp-webpack-plugin')
+config.plugins.push(new DeployPlugin())
 
 webpack(config).watch({
 }, (err, stats) => {
